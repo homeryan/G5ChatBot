2f66264d2e38f9c22bc8c16a1964e4c527289de4
Hong Yan <homer.yan@gmail.com>
2019-04-20 17:51:11 -0600

    Changed .env

--- utils/yelp.js
+++ utils/yelp.js
@@ -1,5 +1,4 @@
 const yelp = require('yelp-fusion');
-const apiKey = 't6s-oG23KMKya65umm9QBUJ0UVEqHYqqdPhm7L6IDzgyl1nDTs3PuTvhBgCmi7DP6FC1amSFaGPzpA5sGdqgSc0GFjovcLKvbF13fdBpwlVSrl9WjLxXb73iUMi4XHYx';
 
 async function search(keyword) {
   const searchRequest = {
@@ -8,7 +7,7 @@ async function search(keyword) {
     limit: 10
   };
 
-  const client = yelp.client(apiKey);
+  const client = yelp.client(process.env.YelpAPIKey);
   const response = await client.search(searchRequest);
   return response.jsonBody.businesses;
 }
